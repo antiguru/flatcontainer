80d5ae2459ade01c2cb78742753bf3368cee39fc
Moritz Hoffmann <antiguru@gmail.com>
2024-02-08 15:47:01 -0500

    Update doc and convenience stuff

--- src/lib.rs
+++ src/lib.rs
@@ -29,6 +29,8 @@
 
 #![deny(missing_docs)]
 
+use std::fmt::{Debug, Formatter};
+
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
 
@@ -119,7 +121,7 @@ pub trait ReserveItems<R: Region> {
 }
 
 /// A container for indices into a region.
-#[derive(Debug, Clone)]
+#[derive(Clone)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[cfg_attr(
     feature = "serde",
@@ -141,6 +143,15 @@ impl<R: Region> Default for FlatStack<R> {
             indices: Vec::default(),
             region: R::default(),
         }
+    }
+}
+
+impl<R: Region> Debug for FlatStack<R>
+where
+    for<'a> R::ReadItem<'a>: Debug,
+{
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.debug_list().entries(self.iter()).finish()
     }
 }
 
@@ -252,6 +263,14 @@ impl<'a, R: Region> ExactSizeIterator for Iter<'a, R> {}
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_readme() {
+        let r: Result<_, u16> = Ok("abc");
+        let mut c = FlatStack::default_impl::<Result<&str, u16>>();
+        c.copy(&r);
+        assert_eq!(r, c.get(0));
+    }
 
     #[test]
     fn test_slice_string_onto() {
@@ -421,6 +440,8 @@ mod tests {
         fn test_copy<T, C: Region + Clone>(t: T)
         where
             T: CopyOnto<C>,
+            // Make sure that types are debug, even if we don't use this in the test.
+            for<'a> C::ReadItem<'a>: Debug,
         {
             let mut c = FlatStack::default();
             c.copy(t);
