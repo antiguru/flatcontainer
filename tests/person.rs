313b5ce4b33fa267563d40bf24e7636a9b87615f
Moritz Hoffmann <antiguru@gmail.com>
2024-05-31 13:41:34 -0400

    Move complex tests to separate folder
    Signed-off-by: Moritz Hoffmann <antiguru@gmail.com>

---
+++ tests/person.rs
@@ -0,0 +1,187 @@
+//! Test a slightly more struct with nested regions, representing people.
+
+use flatcontainer::{Containerized, FlatStack, IntoOwned, Push, Region, ReserveItems};
+
+struct Person {
+    name: String,
+    age: u16,
+    hobbies: Vec<String>,
+}
+
+impl Containerized for Person {
+    type Region = PersonRegion;
+}
+
+#[derive(Default)]
+struct PersonRegion {
+    name_container: <String as Containerized>::Region,
+    age_container: <u16 as Containerized>::Region,
+    hobbies: <Vec<String> as Containerized>::Region,
+}
+
+#[derive(Debug, Clone, Copy)]
+struct PersonRef<'a> {
+    name: <<String as Containerized>::Region as Region>::ReadItem<'a>,
+    age: <<u16 as Containerized>::Region as Region>::ReadItem<'a>,
+    hobbies: <<Vec<String> as Containerized>::Region as Region>::ReadItem<'a>,
+}
+
+impl<'a> IntoOwned<'a> for PersonRef<'a> {
+    type Owned = Person;
+
+    fn into_owned(self) -> Self::Owned {
+        Person {
+            name: self.name.into_owned(),
+            age: self.age,
+            hobbies: self.hobbies.into_owned(),
+        }
+    }
+
+    fn clone_onto(self, other: &mut Self::Owned) {
+        self.name.clone_onto(&mut other.name);
+        other.age = self.age;
+        self.hobbies.clone_onto(&mut other.hobbies);
+    }
+
+    fn borrow_as(owned: &'a Self::Owned) -> Self {
+        Self {
+            name: IntoOwned::borrow_as(&owned.name),
+            age: owned.age,
+            hobbies: IntoOwned::borrow_as(&owned.hobbies),
+        }
+    }
+}
+
+impl Region for PersonRegion {
+    type Owned = Person;
+    type ReadItem<'a> = PersonRef<'a> where Self: 'a;
+    type Index = (
+        <<String as Containerized>::Region as Region>::Index,
+        <<u16 as Containerized>::Region as Region>::Index,
+        <<Vec<String> as Containerized>::Region as Region>::Index,
+    );
+
+    fn merge_regions<'a>(regions: impl Iterator<Item = &'a Self> + Clone) -> Self
+    where
+        Self: 'a,
+    {
+        Self {
+            name_container: <String as Containerized>::Region::merge_regions(
+                regions.clone().map(|r| &r.name_container),
+            ),
+            age_container: <u16 as Containerized>::Region::merge_regions(
+                regions.clone().map(|r| &r.age_container),
+            ),
+            hobbies: <Vec<String> as Containerized>::Region::merge_regions(
+                regions.map(|r| &r.hobbies),
+            ),
+        }
+    }
+
+    fn index(&self, (name, age, hobbies): Self::Index) -> Self::ReadItem<'_> {
+        PersonRef {
+            name: self.name_container.index(name),
+            age: self.age_container.index(age),
+            hobbies: self.hobbies.index(hobbies),
+        }
+    }
+
+    fn reserve_regions<'a, I>(&mut self, regions: I)
+    where
+        Self: 'a,
+        I: Iterator<Item = &'a Self> + Clone,
+    {
+        self.name_container
+            .reserve_regions(regions.clone().map(|r| &r.name_container));
+        self.age_container
+            .reserve_regions(regions.clone().map(|r| &r.age_container));
+        self.hobbies
+            .reserve_regions(regions.clone().map(|r| &r.hobbies));
+    }
+
+    fn clear(&mut self) {
+        self.name_container.clear();
+        self.age_container.clear();
+        self.hobbies.clear();
+    }
+
+    fn heap_size<F: FnMut(usize, usize)>(&self, mut callback: F) {
+        self.name_container.heap_size(&mut callback);
+        self.age_container.heap_size(&mut callback);
+        self.hobbies.heap_size(callback);
+    }
+
+    fn reborrow<'b, 'a: 'b>(item: Self::ReadItem<'a>) -> Self::ReadItem<'b>
+    where
+        Self: 'a,
+    {
+        PersonRef {
+            name: <String as Containerized>::Region::reborrow(item.name),
+            age: <u16 as Containerized>::Region::reborrow(item.age),
+            hobbies: <Vec<String> as Containerized>::Region::reborrow(item.hobbies),
+        }
+    }
+}
+
+impl Push<&Person> for PersonRegion {
+    fn push(&mut self, item: &Person) -> <PersonRegion as Region>::Index {
+        let name = self.name_container.push(&item.name);
+        let age = self.age_container.push(item.age);
+        let hobbies = self.hobbies.push(&item.hobbies);
+        (name, age, hobbies)
+    }
+}
+
+impl<'a> ReserveItems<&'a Person> for PersonRegion {
+    fn reserve_items<I>(&mut self, items: I)
+    where
+        I: Iterator<Item = &'a Person> + Clone,
+    {
+        self.name_container
+            .reserve_items(items.clone().map(|i| &i.name));
+        self.age_container
+            .reserve_items(items.clone().map(|i| &i.age));
+        self.hobbies.reserve_items(items.map(|i| &i.hobbies));
+    }
+}
+
+impl Push<PersonRef<'_>> for PersonRegion {
+    fn push(&mut self, item: PersonRef<'_>) -> <PersonRegion as Region>::Index {
+        let name = self.name_container.push(item.name);
+        let age = self.age_container.push(item.age);
+        let hobbies = self.hobbies.push(item.hobbies);
+        (name, age, hobbies)
+    }
+}
+
+#[test]
+fn test_person() {
+    let hobbies = ["Computers", "Guitar"];
+    let p = Person {
+        name: "Moritz".to_string(),
+        age: 123,
+        hobbies: hobbies.iter().map(ToString::to_string).collect(),
+    };
+
+    let mut c = FlatStack::default_impl::<Person>();
+    c.copy(&p);
+    let person_ref = c.get(0);
+    assert_eq!("Moritz", person_ref.name);
+    assert_eq!(123, person_ref.age);
+    assert_eq!(2, person_ref.hobbies.len());
+    for (copied_hobby, hobby) in person_ref.hobbies.iter().zip(hobbies) {
+        assert_eq!(copied_hobby, hobby);
+    }
+
+    let mut cc = FlatStack::default_impl::<Person>();
+
+    cc.copy(c.get(0));
+
+    let person_ref = cc.get(0);
+    assert_eq!("Moritz", person_ref.name);
+    assert_eq!(123, person_ref.age);
+    assert_eq!(2, person_ref.hobbies.len());
+    for (copied_hobby, hobby) in person_ref.hobbies.iter().zip(hobbies) {
+        assert_eq!(copied_hobby, hobby);
+    }
+}
---
+++ tests/recursive.rs
@@ -0,0 +1,152 @@
+//! Demonstration of how to encode recursive data structures.
+
+use flatcontainer::impls::deduplicate::ConsecutiveOffsetPairs;
+use flatcontainer::impls::offsets::OffsetContainer;
+use flatcontainer::{IntoOwned, Push, Region, StringRegion};
+
+#[derive(Clone)]
+struct List<T>(T, Option<Box<List<T>>>);
+
+struct ListRef<'a, C: Region>(
+    Result<(&'a ListRegion<C>, <C as Region>::Index, Option<usize>), &'a List<C::Owned>>,
+);
+
+impl<'a, C: Region> ListRef<'a, C>
+where
+    C::Owned: Clone,
+{
+    fn inner(&self) -> C::ReadItem<'_> {
+        match &self.0 {
+            Ok((region, index, _continuation)) => region.inner.index(*index),
+            Err(list) => IntoOwned::borrow_as(&list.0),
+        }
+    }
+
+    fn next(&self) -> Option<Self> {
+        match &self.0 {
+            Ok((region, _index, continuation)) => continuation.map(|index| region.index(index)),
+            Err(list) => list
+                .1
+                .as_ref()
+                .map(|next| Self(Err(IntoOwned::borrow_as(next.as_ref())))),
+        }
+    }
+}
+
+impl<'a, C: Region> IntoOwned<'a> for ListRef<'a, C>
+where
+    C::Owned: Clone,
+{
+    type Owned = List<C::Owned>;
+
+    fn into_owned(self) -> Self::Owned {
+        List(
+            self.inner().into_owned(),
+            self.next().map(|next| Box::new(next.into_owned())),
+        )
+    }
+
+    fn clone_onto(self, other: &mut Self::Owned) {
+        *other = self.into_owned();
+    }
+
+    fn borrow_as(owned: &'a Self::Owned) -> Self {
+        Self(Err(owned))
+    }
+}
+
+#[derive(Debug)]
+struct ListRegion<C: Region> {
+    indexes: Vec<(C::Index, Option<usize>)>,
+    inner: C,
+}
+
+impl<C: Region> Default for ListRegion<C> {
+    fn default() -> Self {
+        Self {
+            indexes: Vec::default(),
+            inner: C::default(),
+        }
+    }
+}
+
+impl<C: Region> Region for ListRegion<C>
+where
+    C::Owned: Clone,
+{
+    type Owned = List<C::Owned>;
+    type ReadItem<'a> = ListRef<'a, C> where C: 'a;
+    type Index = usize;
+
+    fn index(&self, index: Self::Index) -> Self::ReadItem<'_> {
+        let (inner_index, continuation) = self.indexes[index];
+        ListRef(Ok((self, inner_index, continuation)))
+    }
+
+    fn merge_regions<'a>(regions: impl Iterator<Item = &'a Self> + Clone) -> Self
+    where
+        Self: 'a,
+    {
+        Self {
+            indexes: Vec::with_capacity(regions.clone().map(|r| r.indexes.len()).sum()),
+            inner: C::merge_regions(regions.map(|r| &r.inner)),
+        }
+    }
+
+    fn reserve_regions<'a, I>(&mut self, regions: I)
+    where
+        Self: 'a,
+        I: Iterator<Item = &'a Self> + Clone,
+    {
+        self.indexes
+            .reserve(regions.clone().map(|r| r.indexes.len()).sum());
+        self.inner.reserve_regions(regions.map(|r| &r.inner));
+    }
+
+    fn clear(&mut self) {
+        self.indexes.clear();
+        self.inner.clear();
+    }
+
+    fn heap_size<F: FnMut(usize, usize)>(&self, mut callback: F) {
+        self.indexes.heap_size(&mut callback);
+        self.inner.heap_size(callback);
+    }
+
+    fn reborrow<'b, 'a: 'b>(item: Self::ReadItem<'a>) -> Self::ReadItem<'b>
+    where
+        Self: 'a,
+    {
+        item
+    }
+}
+
+impl<C, T> Push<&List<T>> for ListRegion<C>
+where
+    for<'a> C: Region + Push<&'a T>,
+    C::Owned: Clone,
+{
+    fn push(&mut self, item: &List<T>) -> <ListRegion<C> as Region>::Index {
+        let inner_index = self.inner.push(&item.0);
+        let continuation = item.1.as_deref().map(|next| self.push(next));
+        self.indexes.push((inner_index, continuation));
+        self.indexes.len() - 1
+    }
+}
+
+#[test]
+fn recursive() {
+    let mut region = <ListRegion<ConsecutiveOffsetPairs<StringRegion>>>::default();
+    let r = List("abc", Some(Box::new(List("def", None))));
+    let index = region.push(&r);
+
+    let rref = region.index(index);
+    assert_eq!(rref.inner(), "abc");
+    let next = rref.next();
+    assert!(next.is_some());
+    let next = next.unwrap();
+    assert_eq!(next.inner(), "def");
+    assert!(next.next().is_none());
+
+    println!("{region:?}");
+}
