712966c6b1c9d7f21084d14139ca238c5a11584d
Moritz Hoffmann <antiguru@gmail.com>
2024-07-15 15:10:31 -0400

    Support bencher and codspeed (#60)
    Signed-off-by: Moritz Hoffmann <antiguru@gmail.com>

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,9 +1,6 @@
 //! A simple benchmark for flatcontainer, adopted from `columnation`'s benchmark.
 
-#![feature(test)]
-
-extern crate test;
-
+use codspeed_bencher_compat::{benchmark_group, benchmark_main, Bencher};
 use flatcontainer::impls::deduplicate::{CollapseSequence, ConsecutiveIndexPairs};
 use flatcontainer::impls::index::IndexOptimized;
 use flatcontainer::impls::tuple::{TupleABCRegion, TupleABRegion};
@@ -11,44 +8,34 @@ use flatcontainer::{
     ColumnsRegion, FlatStack, MirrorRegion, OwnedRegion, Push, Region, RegionPreference,
     ReserveItems, SliceRegion, StringRegion,
 };
-use test::Bencher;
 
-#[bench]
 fn empty_copy(bencher: &mut Bencher) {
     _bench_copy(bencher, vec![(); 1024]);
 }
-#[bench]
 fn u64_copy(bencher: &mut Bencher) {
     _bench_copy(bencher, vec![0u64; 1024]);
 }
-#[bench]
 fn u32x2_copy(bencher: &mut Bencher) {
     _bench_copy(bencher, vec![(0u32, 0u32); 1024]);
 }
-#[bench]
 fn u8_u64_copy(bencher: &mut Bencher) {
     _bench_copy(bencher, vec![(0u8, 0u64); 512]);
 }
-#[bench]
 fn str10_copy(bencher: &mut Bencher) {
     _bench_copy(bencher, vec!["grawwwwrr!"; 1024]);
 }
-#[bench]
 fn string10_copy(bencher: &mut Bencher) {
     _bench_copy(bencher, vec![format!("grawwwwrr!"); 1024]);
 }
-#[bench]
 fn string20_copy(bencher: &mut Bencher) {
     _bench_copy(bencher, vec![format!("grawwwwrr!!!!!!!!!!!"); 512]);
 }
-#[bench]
 fn vec_u_s_copy(bencher: &mut Bencher) {
     _bench_copy(
         bencher,
         vec![vec![(0u64, "grawwwwrr!".to_string()); 32]; 32],
     );
 }
-#[bench]
 fn vec_u_vn_s_copy(bencher: &mut Bencher) {
     _bench_copy(
         bencher,
@@ -56,56 +43,45 @@ fn vec_u_vn_s_copy(bencher: &mut Bencher) {
     );
 }
 
-#[bench]
 fn empty_copy_region(bencher: &mut Bencher) {
     _bench_copy_region::<OwnedRegion<_>, _>(bencher, vec![(); 1024]);
 }
-#[bench]
 fn u64_copy_region(bencher: &mut Bencher) {
     _bench_copy_region::<OwnedRegion<_>, _>(bencher, vec![0u64; 1024]);
 }
-#[bench]
 fn u32x2_copy_region(bencher: &mut Bencher) {
     _bench_copy_region::<OwnedRegion<_>, _>(bencher, vec![(0u32, 0u32); 1024]);
 }
-#[bench]
 fn u8_u64_copy_region(bencher: &mut Bencher) {
     _bench_copy_region::<OwnedRegion<_>, _>(bencher, vec![(0u8, 0u64); 512]);
 }
-#[bench]
 fn str10_copy_region(bencher: &mut Bencher) {
     _bench_copy_region::<OwnedRegion<_>, _>(bencher, vec!["grawwwwrr!"; 1024]);
 }
-#[bench]
 fn str100_copy_region(bencher: &mut Bencher) {
     _bench_copy_region::<OwnedRegion<_>, _>(bencher, vec!["grawwwwrrgrawwwwrrgrawwwwrrgrawwwwrrgrawwwwrrgrawwwwrrgrawwwwrrgrawwwwrrgrawwwwrr!!!!!!!!!grawwwwrr!"; 1024]);
 }
-#[bench]
 fn string10_copy_region(bencher: &mut Bencher) {
     _bench_copy_region::<SliceRegion<StringRegion>, _>(bencher, vec![format!("grawwwwrr!"); 1024]);
 }
-#[bench]
 fn string10_copy_region_collapse(bencher: &mut Bencher) {
     _bench_copy_region::<
         SliceRegion<CollapseSequence<ConsecutiveIndexPairs<StringRegion>>, IndexOptimized>,
         _,
     >(bencher, vec![format!("grawwwwrr!"); 1024]);
 }
-#[bench]
 fn string20_copy_region(bencher: &mut Bencher) {
     _bench_copy_region::<SliceRegion<StringRegion>, _>(
         bencher,
         vec![format!("grawwwwrr!!!!!!!!!!!"); 512],
     );
 }
-#[bench]
 fn vec_u_s_copy_region(bencher: &mut Bencher) {
     _bench_copy_region::<SliceRegion<SliceRegion<TupleABRegion<MirrorRegion<_>, StringRegion>>>, _>(
         bencher,
         vec![vec![(0u64, "grawwwwrr!".to_string()); 32]; 32],
     );
 }
-#[bench]
 fn vec_u_vn_s_copy_region(bencher: &mut Bencher) {
     _bench_copy_region::<
         SliceRegion<SliceRegion<TupleABCRegion<MirrorRegion<_>, OwnedRegion<_>, StringRegion>>>,
@@ -115,7 +91,6 @@ fn vec_u_vn_s_copy_region(bencher: &mut Bencher) {
         vec![vec![(0u64, vec![(); 1 << 40], "grawwwwrr!".to_string()); 32]; 32],
     );
 }
-#[bench]
 fn vec_u_vn_s_copy_region_column(bencher: &mut Bencher) {
     _bench_copy_region::<
         SliceRegion<
@@ -134,42 +109,33 @@ fn vec_u_vn_s_copy_region_column(bencher: &mut Bencher) {
     );
 }
 
-#[bench]
 fn empty_clone(bencher: &mut Bencher) {
     _bench_clone(bencher, vec![(); 1024]);
 }
-#[bench]
 fn u64_clone(bencher: &mut Bencher) {
     _bench_clone(bencher, vec![0u64; 1024]);
 }
-#[bench]
 fn u32x2_clone(bencher: &mut Bencher) {
     _bench_clone(bencher, vec![(0u32, 0u32); 1024]);
 }
-#[bench]
 fn u8_u64_clone(bencher: &mut Bencher) {
     _bench_clone(bencher, vec![(0u8, 0u64); 512]);
 }
-#[bench]
 fn str10_clone(bencher: &mut Bencher) {
     _bench_clone(bencher, vec!["grawwwwrr!"; 1024]);
 }
-#[bench]
 fn string10_clone(bencher: &mut Bencher) {
     _bench_clone(bencher, vec![format!("grawwwwrr!"); 1024]);
 }
-#[bench]
 fn string20_clone(bencher: &mut Bencher) {
     _bench_clone(bencher, vec![format!("grawwwwrr!!!!!!!!!!!"); 512]);
 }
-#[bench]
 fn vec_u_s_clone(bencher: &mut Bencher) {
     _bench_clone(
         bencher,
         vec![vec![(0u64, "grawwwwrr!".to_string()); 32]; 32],
     );
 }
-#[bench]
 fn vec_u_vn_s_clone(bencher: &mut Bencher) {
     _bench_clone(
         bencher,
@@ -177,42 +143,33 @@ fn vec_u_vn_s_clone(bencher: &mut Bencher) {
     );
 }
 
-#[bench]
 fn empty_realloc(bencher: &mut Bencher) {
     _bench_realloc(bencher, vec![(); 1024]);
 }
-#[bench]
 fn u64_realloc(bencher: &mut Bencher) {
     _bench_realloc(bencher, vec![0u64; 1024]);
 }
-#[bench]
 fn u32x2_realloc(bencher: &mut Bencher) {
     _bench_realloc(bencher, vec![(0u32, 0u32); 1024]);
 }
-#[bench]
 fn u8_u64_realloc(bencher: &mut Bencher) {
     _bench_realloc(bencher, vec![(0u8, 0u64); 512]);
 }
-#[bench]
 fn str10_realloc(bencher: &mut Bencher) {
     _bench_realloc(bencher, vec!["grawwwwrr!"; 1024]);
 }
-#[bench]
 fn string10_realloc(bencher: &mut Bencher) {
     _bench_realloc(bencher, vec![format!("grawwwwrr!"); 1024]);
 }
-#[bench]
 fn string20_realloc(bencher: &mut Bencher) {
     _bench_realloc(bencher, vec![format!("grawwwwrr!!!!!!!!!!!"); 512]);
 }
-#[bench]
 fn vec_u_s_realloc(bencher: &mut Bencher) {
     _bench_realloc(
         bencher,
         vec![vec![(0u64, "grawwwwrr!".to_string()); 32]; 32],
     );
 }
-#[bench]
 fn vec_u_vn_s_realloc(bencher: &mut Bencher) {
     _bench_realloc(
         bencher,
@@ -220,42 +177,33 @@ fn vec_u_vn_s_realloc(bencher: &mut Bencher) {
     );
 }
 
-#[bench]
 fn empty_prealloc(bencher: &mut Bencher) {
     _bench_prealloc(bencher, vec![(); 1024]);
 }
-#[bench]
 fn u64_prealloc(bencher: &mut Bencher) {
     _bench_prealloc(bencher, vec![0u64; 1024]);
 }
-#[bench]
 fn u32x2_prealloc(bencher: &mut Bencher) {
     _bench_prealloc(bencher, vec![(0u32, 0u32); 1024]);
 }
-#[bench]
 fn u8_u64_prealloc(bencher: &mut Bencher) {
     _bench_prealloc(bencher, vec![(0u8, 0u64); 512]);
 }
-#[bench]
 fn str10_prealloc(bencher: &mut Bencher) {
     _bench_prealloc(bencher, vec!["grawwwwrr!"; 1024]);
 }
-#[bench]
 fn string10_prealloc(bencher: &mut Bencher) {
     _bench_prealloc(bencher, vec![format!("grawwwwrr!"); 1024]);
 }
-#[bench]
 fn string20_prealloc(bencher: &mut Bencher) {
     _bench_prealloc(bencher, vec![format!("grawwwwrr!!!!!!!!!!!"); 512]);
 }
-#[bench]
 fn vec_u_s_prealloc(bencher: &mut Bencher) {
     _bench_prealloc(
         bencher,
         vec![vec![(0u64, "grawwwwrr!".to_string()); 32]; 32],
     );
 }
-#[bench]
 fn vec_u_vn_s_prealloc(bencher: &mut Bencher) {
     _bench_prealloc(
         bencher,
@@ -263,46 +211,36 @@ fn vec_u_vn_s_prealloc(bencher: &mut Bencher) {
     );
 }
 
-#[bench]
 fn empty_copy_flat(bencher: &mut Bencher) {
     _bench_copy_flat_preference(bencher, vec![(); 1024]);
 }
-#[bench]
 fn u64_copy_flat(bencher: &mut Bencher) {
     _bench_copy_flat_preference(bencher, vec![0u64; 1024]);
 }
-#[bench]
 fn u32x2_copy_flat(bencher: &mut Bencher) {
     _bench_copy_flat_preference(bencher, vec![(0u32, 0u32); 1024]);
 }
-#[bench]
 fn u8_u64_copy_flat(bencher: &mut Bencher) {
     _bench_copy_flat_preference(bencher, vec![(0u8, 0u64); 512]);
 }
-#[bench]
 fn str10_copy_flat(bencher: &mut Bencher) {
     _bench_copy_flat_preference(bencher, vec!["grawwwwrr!"; 1024]);
 }
-#[bench]
 fn str100_copy_flat(bencher: &mut Bencher) {
     _bench_copy_flat_preference(bencher, vec!["grawwwwrrgrawwwwrrgrawwwwrrgrawwwwrrgrawwwwrrgrawwwwrrgrawwwwrrgrawwwwrrgrawwwwrr!!!!!!!!!grawwwwrr!"; 1024]);
 }
-#[bench]
 fn string10_copy_flat(bencher: &mut Bencher) {
     _bench_copy_flat_preference(bencher, vec![format!("grawwwwrr!"); 1024]);
 }
-#[bench]
 fn string20_copy_flat(bencher: &mut Bencher) {
     _bench_copy_flat_preference(bencher, vec![format!("grawwwwrr!!!!!!!!!!!"); 512]);
 }
-#[bench]
 fn vec_u_s_copy_flat(bencher: &mut Bencher) {
     _bench_copy_flat_preference(
         bencher,
         vec![vec![(0u64, "grawwwwrr!".to_string()); 32]; 32],
     );
 }
-#[bench]
 fn vec_u_vn_s_copy_flat(bencher: &mut Bencher) {
     _bench_copy_flat_preference(
         bencher,
@@ -310,49 +248,39 @@ fn vec_u_vn_s_copy_flat(bencher: &mut Bencher) {
     );
 }
 
-#[bench]
 fn empty_copy_flat_region(bencher: &mut Bencher) {
     _bench_copy_flat::<OwnedRegion<_>, _>(bencher, vec![(); 1024]);
 }
-#[bench]
 fn u64_copy_flat_region(bencher: &mut Bencher) {
     _bench_copy_flat::<OwnedRegion<_>, _>(bencher, vec![0u64; 1024]);
 }
-#[bench]
 fn u32x2_copy_flat_region(bencher: &mut Bencher) {
     _bench_copy_flat::<OwnedRegion<_>, _>(bencher, vec![(0u32, 0u32); 1024]);
 }
-#[bench]
 fn u8_u64_copy_flat_region(bencher: &mut Bencher) {
     _bench_copy_flat::<OwnedRegion<_>, _>(bencher, vec![(0u8, 0u64); 512]);
 }
-#[bench]
 fn str10_copy_flat_region(bencher: &mut Bencher) {
     _bench_copy_flat::<OwnedRegion<_>, _>(bencher, vec!["grawwwwrr!"; 1024]);
 }
-#[bench]
 fn str100_copy_flat_region(bencher: &mut Bencher) {
     _bench_copy_flat::<OwnedRegion<_>, _>(bencher, vec!["grawwwwrrgrawwwwrrgrawwwwrrgrawwwwrrgrawwwwrrgrawwwwrrgrawwwwrrgrawwwwrrgrawwwwrr!!!!!!!!!grawwwwrr!"; 1024]);
 }
-#[bench]
 fn string10_copy_flat_region(bencher: &mut Bencher) {
     _bench_copy_flat::<SliceRegion<StringRegion>, _>(bencher, vec![format!("grawwwwrr!"); 1024]);
 }
-#[bench]
 fn string20_copy_flat_region(bencher: &mut Bencher) {
     _bench_copy_flat::<SliceRegion<StringRegion>, _>(
         bencher,
         vec![format!("grawwwwrr!!!!!!!!!!!"); 512],
     );
 }
-#[bench]
 fn vec_u_s_copy_flat_region(bencher: &mut Bencher) {
     _bench_copy_flat::<SliceRegion<SliceRegion<TupleABRegion<MirrorRegion<_>, StringRegion>>>, _>(
         bencher,
         vec![vec![(0u64, "grawwwwrr!".to_string()); 32]; 32],
     );
 }
-#[bench]
 fn vec_u_vn_s_copy_flat_region(bencher: &mut Bencher) {
     _bench_copy_flat::<
         SliceRegion<SliceRegion<TupleABCRegion<MirrorRegion<_>, OwnedRegion<_>, StringRegion>>>,
@@ -362,7 +290,6 @@ fn vec_u_vn_s_copy_flat_region(bencher: &mut Bencher) {
         vec![vec![(0u64, vec![(); 1 << 40], "grawwwwrr!".to_string()); 32]; 32],
     );
 }
-#[bench]
 fn vec_u_vn_s_copy_flat_region_column(bencher: &mut Bencher) {
     _bench_copy_flat::<
         SliceRegion<
@@ -511,3 +438,89 @@ where
     });
     set_bytes(&mut bencher.bytes, siz);
 }
+
+benchmark_group!(
+    clone,
+    empty_clone,
+    str10_clone,
+    string10_clone,
+    string20_clone,
+    u32x2_clone,
+    u64_clone,
+    u8_u64_clone,
+    vec_u_s_clone,
+    vec_u_vn_s_clone,
+);
+benchmark_group!(
+    copy,
+    empty_copy,
+    str10_copy,
+    string10_copy,
+    string20_copy,
+    u32x2_copy,
+    u64_copy,
+    u8_u64_copy,
+    vec_u_s_copy,
+    vec_u_vn_s_copy,
+);
+benchmark_group!(
+    copy_flat,
+    empty_copy_flat,
+    str100_copy_flat,
+    str10_copy_flat,
+    string10_copy_flat,
+    string20_copy_flat,
+    u32x2_copy_flat,
+    u64_copy_flat,
+    u8_u64_copy_flat,
+    vec_u_s_copy_flat,
+    vec_u_vn_s_copy_flat,
+);
+benchmark_group!(
+    copy_region,
+    empty_copy_flat_region,
+    empty_copy_region,
+    str100_copy_flat_region,
+    str100_copy_region,
+    str10_copy_flat_region,
+    str10_copy_region,
+    string10_copy_flat_region,
+    string10_copy_region,
+    string10_copy_region_collapse,
+    string20_copy_flat_region,
+    string20_copy_region,
+    u32x2_copy_flat_region,
+    u32x2_copy_region,
+    u64_copy_flat_region,
+    u64_copy_region,
+    u8_u64_copy_flat_region,
+    u8_u64_copy_region,
+    vec_u_s_copy_flat_region,
+    vec_u_s_copy_region,
+    vec_u_vn_s_copy_flat_region,
+    vec_u_vn_s_copy_flat_region_column,
+    vec_u_vn_s_copy_region,
+    vec_u_vn_s_copy_region_column,
+);
+benchmark_group!(
+    alloc,
+    empty_prealloc,
+    empty_realloc,
+    str10_prealloc,
+    str10_realloc,
+    string10_prealloc,
+    string10_realloc,
+    string20_prealloc,
+    string20_realloc,
+    u32x2_prealloc,
+    u32x2_realloc,
+    u64_prealloc,
+    u64_realloc,
+    u8_u64_prealloc,
+    u8_u64_realloc,
+    vec_u_s_prealloc,
+    vec_u_s_realloc,
+    vec_u_vn_s_prealloc,
+    vec_u_vn_s_realloc,
+);
+benchmark_main!(clone, copy, copy_flat, copy_region, alloc);
